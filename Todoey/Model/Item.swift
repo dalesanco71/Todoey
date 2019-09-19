10aef88087efaee039337093a920d44b4d851ad7
Daniel Alesanco <dalesanco@gmail.com>
2019-09-19 08:17:24 +0200

    Order files in MVC schema

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Daniel Alesanco on 18/09/2019.
+//  Copyright © 2019 Daniel Alesanco. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+
+    var title    = ""
+    var done     = false
+    
+}
